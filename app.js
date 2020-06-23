3d8862233206adb50f756bdd136a511f217be489
bubble <470818974@qq.com>
2020-06-23 17:19:57 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const app=require('express')();
+const server=require('http').Server(app);
+var io=require('socket.io')(server);
+
+const users=[];
+
+server.listen(3000,()=>{
+	console.log('server OK...');
+})
+
+app.use(require('express').static('public'));
+
+app.get('/',(req,res)=>{
+	res.redirect('/index.html');
+})
+
+io.on('connection',(socket)=>{
+	console.log('新用户连接了');
+	socket.on('login',function(data){
+		var user=users.find(item=>item.username===data.username);
+		if(user){
+			socket.emit('loginError',{msg:'用户名已存在'});
+		}else{
+			users.push(data);
+			socket.emit('loginSuccess',data);
+			
+			//广播新用户
+			io.emit('addUser',data);
+			//把用户列表发送给每个用户
+			io.emit('userList',users);
+			
+			socket.username=data.username;
+			socket.avatar=data.avatar;
+			
+		}
+	})
+	
+	socket.on('disconnect',()=>{
+		let idx=users.findIndex(item=>{item.username==socket.username});
+		users.splice(idx,1);
+		io.emit('deleteUser',{
+			username:socket.username
+		});
+		io.emit('userList',users);
+	})
+	
+	socket.on('sendMessage',data=>{
+		io.emit('receiveMessage',data);
+	})
+	
+	socket.on('sendImage',data=>{
+		io.emit('receiveImage',data);
+	})
+})
---
+++ public/js/index.js
@@ -0,0 +1,217 @@
+var socket=io('http://localhost:3000');
+
+var username='';
+var avatar='';
+//登录
+$('#login_avatar li').on('click',function(){
+	$(this).addClass('current').siblings().removeClass('current');
+});
+$('#loginBtn').on('click',function(){
+	var username=$('#username').val().trim();
+	if(!username){
+		alert('请输入用户名');
+		return;
+	}
+	var avatar=$('#login_avatar li.current img').attr('src');
+	
+	socket.emit('login',{
+		username:username,
+		avatar:avatar
+	})
+})
+
+socket.on('loginError',function(data){
+	alert(data.msg);
+});
+socket.on('loginSuccess',function(data){
+	$('.login_box').fadeOut();
+	$('.container').fadeIn();
+	
+	$('.avatar_url').attr('src',data.avatar);
+	$('.info .username').text(data.username);
+	
+	username=data.username;
+	avatar=data.avatar;
+});
+
+socket.on('addUser',function(data){
+	$('.chatroom_bd').append(`
+		<div class="system">
+			<p class="message_system">
+				<span class="content">${data.username}加入了群聊</span>
+			</p>
+		</div>
+	`)
+	scrollIntoView();
+});
+
+socket.on('deleteUser',function(data){
+	$('.chatroom_bd').append(`
+		<div class="system">
+			<p class="message_system">
+				<span class="content">${data.username}离开了群聊</span>
+			</p>
+		</div>
+	`)
+	scrollIntoView();
+});
+
+socket.on('userList',data=>{
+	$('#users').html('');
+	data.forEach(item=>{
+		$('#users').append(`
+			<li class="user">
+				<div class="avatar"><img src=${item.avatar} ></div>
+				<div class="name">
+					<h3 class="username">${item.username}</h3>
+				</div>
+			</li>
+		`)
+	});
+	
+	$('#user_count').text(data.length);
+})
+
+
+$(document).keydown (function(e) {
+    if(e.keyCode == 13) {
+        send();
+		return false;
+    }
+})
+$('#sendBtn').on('click',()=>{
+    send();
+})
+function send(){
+	var content=$('#content').html().trim();
+	$('#content').html('');
+	if(!content){
+		return alert('请输入内容');
+	}
+	socket.emit('sendMessage',{
+		msg:content,
+		username:username,
+		avatar:avatar
+	})
+}
+
+
+socket.on('receiveMessage',data=>{
+	if(data.username===username){
+		$('.chatroom_bd').append(`
+			<div class="message_box">
+				<div class="my message">
+					<img src="${data.avatar}" class="avatar">
+					<div class="content">
+						<div class="bubble">
+							<div class="bubble_count">
+								${data.msg}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`)
+	}else{
+		$('.chatroom_bd').append(`
+			<div class="message_box">
+				<div class="other message">
+					<img src="${data.avatar}" class="avatar" >
+					<div class="content">
+						<div class="nickname">${data.username}</div>
+						<div class="bubble">
+							<div class="bubble_count">
+								${data.msg}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`)
+	}
+	
+	scrollIntoView();
+	
+})
+
+
+function scrollIntoView(){
+	//滚动到底部
+	$('.chatroom_bd').children(':last').get(0).scrollIntoView(false);
+	
+}
+
+
+//发送图片
+$('#file').on('change',function(){
+	var file=this.files[0];
+	var fr=new FileReader();
+	fr.readAsDataURL(file);
+	fr.onload=function(){
+		socket.emit('sendImage',{
+			username:username,
+			avatar:avatar,
+			img:fr.result
+		})
+	}
+})
+
+socket.on('receiveImage',data=>{
+	if(data.username===username){
+		$('.chatroom_bd').append(`
+			<div class="message_box">
+				<div class="my message">
+					<img src="${data.avatar}" class="avatar">
+					<div class="content">
+						<div class="bubble">
+							<div class="bubble_count">
+								<img src="${data.img}">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`)
+	}else{
+		$('.chatroom_bd').append(`
+			<div class="message_box">
+				<div class="other message">
+					<img src="${data.avatar}" class="avatar" >
+					<div class="content">
+						<div class="nickname">${data.username}</div>
+						<div class="bubble">
+							<div class="bubble_count">
+								<img src="${data.img}">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`)
+	}
+	
+	$('.chatroom_bd img:last').on('load',()=>{
+		scrollIntoView();
+	})
+	
+})
+
+//表情
+// $('.face').on('click',function(){
+// 	$('#content').emoji({
+// 		button:'.face',
+// 		showTab:false,
+// 		animation:'slide',
+// 		position:'topLeft',
+// 		icons:[
+// 			{
+// 				name:'QQ表情',
+// 				path:'lib/jquery-emoji/img/qq/',
+// 				maxNum:91,
+// 				excludeNums:[41,45,54],
+// 				file:'gif'
+				
+// 			}
+// 		]
+// 	})
+// })
